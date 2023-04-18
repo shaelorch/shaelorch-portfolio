fb469187786de61b3c27888bee8d6a8a6030bc15
Shae Lorch <slorch@my.bcit.ca>
2023-04-18 13:13:18 -0700

    responsive to mobile

---
+++ .history/components/switch_20230417174046.js
@@ -0,0 +1,42 @@
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
+import Link from "next/link";
+
+
+export default function Switch() {
+
+    const [active, setActive] = useState(false);
+    const [disable, setDisable] = useState(false);
+
+    const handleClick = () => {
+        setActive(true);
+   
+    }
+
+    const handleClickClose = () => {
+        setActive(false);
+      
+
+        
+    }
+
+   
+
+    return (
+        <><div className={styles.switchcont}>
+            <div className={styles.switch}>
+                <Link href="/aboutme">
+                    <div className={styles.switchbut}></div>
+                </Link>
+            </div>
+
+        </div><div className={styles.switchcont}>
+                <div className={styles.switch}>
+                    <Link href="/aboutme">
+                        <div className={styles.switchbut}></div>
+                    </Link>
+                </div>
+
+            </div></>
+    )
+}
